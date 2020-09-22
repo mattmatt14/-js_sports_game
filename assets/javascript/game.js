c4331434bd9c8082a8190105aebad9427d111c64
mattmatt14 <matthewprather14@gmail.com>
2020-09-21 20:17:53 -0700

    we tried sad

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+let shootButton1 = document.querySelector("#teamone-shoot-button");
+let teamone = document.querySelector("#teamone-numshots");
+let stringTeamoneValue = teamone.innerHTML;
+let teamoneValue = Number(stringTeamoneValue);
+console.log(teamoneValue);
+console.log(stringTeamoneValue);
+
+shootButton1.addEventListener("click", function() {
+    teamoneValue +=1;
+    console.log(teamoneValue);
+    teamone.innerHTML = teamoneValue;
+    
+
+})
+
+let shootButton2 = document.querySelector("#teamtwo-shoot-button");
+let teamtwo = document.querySelector("#teamtwo-numshots");
+let stringTeamtwoValue = teamtwo.innerHTML;
+let teamtwoValue = Number(stringTeamtwoValue);
+console.log(teamtwoValue);
+console.log(stringTeamtwoValue);
+
+
+
+shootButton2.addEventListener("click", function() {
+    teamtwoValue +=1;
+    console.log(teamtwoValue);
+    teamtwo.innerHTML = teamtwoValue;
+
+
+})   
+
+
+  
+    
+
